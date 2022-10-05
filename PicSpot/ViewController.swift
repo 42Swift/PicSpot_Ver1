c5bf0a6dc0a2ccad676f6ab036df6e22db6ae149
zhunhe <xuzhunhe@gmail.com>
2022-10-05 12:29:12 +0900

    Initial Commit

---
+++ PicSpot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PicSpot
+//
+//  Created by jun on 2022/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
