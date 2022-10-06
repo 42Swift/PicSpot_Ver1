50ed134ad05522c0bb9039da7f2ee9fb8670c8c6
zhunhe <xuzhunhe@gmail.com>
2022-10-06 13:43:40 +0900

    feat: 하단 탭 바 구현 #1

---
+++ PicSpot/Controllers/ClubViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ClubViewController.swift
+//  PicSpot
+//
+//  Created by jun on 2022/10/06.
+//
+
+import UIKit
+
+class ClubViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+
+}
---
+++ PicSpot/Controllers/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  PicSpot
+//
+//  Created by jun on 2022/10/06.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+}
