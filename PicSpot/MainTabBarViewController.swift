d9c49f485a9e36c8bad6aaee0ff84ad5c61207e2
zhunhe <xuzhunhe@gmail.com>
2022-10-07 14:21:23 +0900

    feat: 탭바 배경 색상, 타이틀 설정
    - 탭바 배경 색상을 지정했습니다 (.systemBackground)
    - 탭바 아이템에 타이틀을 지정했습니다 (Spot, Map, Club, Profile)
    - MainTabBarViewController.swift에 주석을 추가했습니다
    - 불필요한 view.backgroundColor를 삭제했습니다

--- PicSpot/MainTabBarViewController.swift
+++ PicSpot/MainTabBarViewController.swift
@@ -11,20 +11,31 @@ class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
 
+        // 뷰컨트롤러 연결
         let vc1 = UINavigationController(rootViewController: SpotViewController())
         let vc2 = UINavigationController(rootViewController: MapViewController())
         let vc3 = UINavigationController(rootViewController: ClubViewController())
         let vc4 = UINavigationController(rootViewController: ProfileViewController())
 
+        // 탭바 아이콘 sf symbols로 설정
         vc1.tabBarItem.image = UIImage(systemName: "camera.on.rectangle.fill")
         vc2.tabBarItem.image = UIImage(systemName: "map")
         vc3.tabBarItem.image = UIImage(systemName: "person.2.fill")
         vc4.tabBarItem.image = UIImage(systemName: "person.crop.circle")
 
-        tabBar.tintColor = .label
+        // 탭바 아이템 타이틀 설정
+        vc1.title = "Spot"
+        vc2.title = "Map"
+        vc3.title = "Club"
+        vc4.title = "Profile"
 
+        // 탭바 아이템 선택시의 색상
+        tabBar.tintColor = .label
+        // 탭바 배경 색상
+        tabBar.backgroundColor = .systemBackground
+
+        // 탭바 아이템 표시
         setViewControllers([vc1, vc2, vc3, vc4], animated: true)
     }
 
