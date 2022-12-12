2f1f59f1f7d786d9459e61bf658362e16099ac33
zhunhe <xuzhunhe@gmail.com>
2022-12-12 21:29:41 +0900

    feat: ClubViewController 삭제

--- PicSpot/Controllers/MainTabBarViewController.swift
+++ PicSpot/Controllers/MainTabBarViewController.swift
@@ -11,8 +11,7 @@ class MainTabBarViewController: UITabBarController {
     enum ViewController: Int {
         case spot = 0
         case map = 1
-        case club = 2
-        case profile = 3
+        case profile = 2
     }
 
     override func viewDidLoad() {
@@ -20,20 +19,17 @@ class MainTabBarViewController: UITabBarController {
         // 뷰컨트롤러 연결
         let vc1 = UINavigationController(rootViewController: SpotViewController())
         let vc2 = UINavigationController(rootViewController: MapViewController())
-        let vc3 = UINavigationController(rootViewController: ClubViewController())
-        let vc4 = UINavigationController(rootViewController: ProfileViewController())
+        let vc3 = UINavigationController(rootViewController: ProfileViewController())
 
         // 탭바 아이콘 sf symbols로 설정
         vc1.tabBarItem.image = UIImage(systemName: "camera.on.rectangle.fill")
         vc2.tabBarItem.image = UIImage(systemName: "map")
-        vc3.tabBarItem.image = UIImage(systemName: "person.2.fill")
-        vc4.tabBarItem.image = UIImage(systemName: "person.crop.circle")
+        vc3.tabBarItem.image = UIImage(systemName: "person.crop.circle")
 
         // 탭바 아이템 타이틀 설정
         vc1.title = "Spot"
         vc2.title = "Map"
-        vc3.title = "Club"
-        vc4.title = "Profile"
+        vc3.title = "Profile"
 
         // 탭바 아이템 선택시의 색상
         tabBar.tintColor = .label
@@ -41,7 +37,7 @@ class MainTabBarViewController: UITabBarController {
         tabBar.backgroundColor = .systemBackground
 
         // 탭바 아이템 표시
-        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
+        setViewControllers([vc1, vc2, vc3], animated: true)
         self.selectedIndex = ViewController.spot.rawValue
 
     }
